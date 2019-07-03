6773896418c1cf471e49ec5b7a88d5181e3dde3b
Autumn R. Mosley <36770891+hatbearingoctopus@users.noreply.github.com>
2019-07-02 21:18:13 -0700

    Create reverseAString.js

---
+++ reverseAString.js
@@ -0,0 +1,6 @@
+// Reverse the provided string. You may need to turn the string into an array. Result must be a string.
+
+function reverseString (str) {
+  return str.split('').reverse().join('');
+  
+  //split string apart between characters, use reverse to reverse it and join to join back together and return it all
