53b395a6bb6aa56fe3430e56e8e5544bf0e38c43
Autumn R. Mosley <36770891+hatbearingoctopus@users.noreply.github.com>
2019-07-03 21:16:24 -0700

    Create booWho.js

---
+++ booWho.js
@@ -0,0 +1,8 @@
+// Check if a value is classified as a boolean. Return true or false. 
+
+function booWho (bool) {
+
+return typeOf bool === 'boolean';
+}
+
+//typeOf operator returns a string indicating the type of the unevaluated operand. 
