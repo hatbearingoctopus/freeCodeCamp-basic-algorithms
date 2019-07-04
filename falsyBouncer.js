3574a899638c6f13a8bab9f45ab5a9f679025a3e
Autumn R. Mosley <36770891+hatbearingoctopus@users.noreply.github.com>
2019-07-03 21:28:44 -0700

    Create falsyBouncer.js

---
+++ falsyBouncer.js
@@ -0,0 +1,7 @@
+// Remove all falsy values from an array (false, null, 0, "", NaN, undefined).
+
+function bouncer (arr) {
+  return arr.filter(boolean);
+ }
+ 
+ //Used built in filter method to search for boolean, then returned.
